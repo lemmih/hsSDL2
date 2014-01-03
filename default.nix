63a5278a5a447b66c03e7fab714c753c4757cf8c
Oliver Charles <ollie@ocharles.org.uk>
2014-01-03 09:08:09 +0000

    Simplify default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,12 @@
 { }:
 with import <nixpkgs> { }; 
 let
-  inherit (haskellPackages) cabal cabalInstall ghcMod vector;
-  SDL2 = callPackage <nixpkgs/pkgs/development/libraries/SDL/SDL2.nix> {};
+  inherit (haskellPackages_ghc763_profiling) cabal cabalInstall ghcMod vector text;
 
 in cabal.mkDerivation (self: {
   pname = "SDL2";
   version = "1.0.0";
   src = ./.;
-  buildDepends = [ SDL2 vector ];
-  buildTools = [ cabalInstall vim_configurable git fish ghcMod less ];
+  buildDepends = [ SDL2 vector text ];
+  buildTools = [ cabalInstall ];
 })
